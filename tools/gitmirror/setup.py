1a0d2fd0bb0bc3c08844d490a14a984170242809
yunhong jiang <yunhong.jiang@intel.com>
2015-01-06 23:32:19 -0800

    Begin learning package, at least work for setup develop now and can be import

---
+++ tools/gitmirror/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(name='gitmirror',
+    version='0.0.1',
+    description='A tools to setup mirror for all openstack git projects',
+    url='http://github.com/jiangyh/worktree',
+    author='Yunhong Jiang',
+    author_email="yunhong.jiang@intel.com",
+    license='MIT',
+    packages=['gitmirror'],
+    zip_safe=False)
