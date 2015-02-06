dc57cd150242dddbfd8f1e60c4d5e694cb61597f
yunhong jiang <yunhong.jiang@intel.com>
2015-02-06 14:41:55 -0800

    For create local vm devstack
    Signed-off-by: yunhong jiang <yunhong.jiang@intel.com>

---
+++ openstack-env/vm_devstack/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#scp  yjiang5@otccloud06.sc.intel.com:/home/yjiang5/.ssh/* ~/.ssh/
+sudo chown ubuntu /opt
+sudo chgrp ubuntu /opt
+sudo scp yjiang5@172.25.110.34:/etc/apt/sources.list /etc/apt/
+sudo apt-get install git
+cd /opt
+git clone ssh://yjiang5@172.25.110.34:/home/yjiang5/GITMIRROR/openstack-dev/devstack
+cd devstack
+mkdir files/images/
+scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/images/* ./files/images/
+scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/get-pip.py ./files/
+
