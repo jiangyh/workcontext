7651d0aa3f2fe19dc633a7ef1612ca3a5499a5b1
yunhong jiang <yunhong.jiang@intel.com>
2015-02-09 17:22:26 -0800

    more files

--- openstack-env/vm_devstack/start.sh
+++ openstack-env/vm_devstack/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#scp  yjiang5@otccloud06.sc.intel.com:/home/yjiang5/.ssh/* ~/.ssh/
+#The comand to login to the guest
+#nova ssh --private --login ubuntu -i ~/tmp/123.key controller
+scp  yjiang5@otccloud06.sc.intel.com:/home/yjiang5/.ssh/* ~/.ssh/
 sudo chown ubuntu /opt
 sudo chgrp ubuntu /opt
 sudo scp yjiang5@172.25.110.34:/etc/apt/sources.list /etc/apt/
+sudo scp yjiang5@172.25.110.34:/etc/pip.conf /etc/
 sudo apt-get install git
 cd /opt
 git clone ssh://yjiang5@172.25.110.34:/home/yjiang5/GITMIRROR/openstack-dev/devstack
