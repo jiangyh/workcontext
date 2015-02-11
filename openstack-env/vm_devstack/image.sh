487c37512f474ea59dd0630732911429885fbbdc
yunhong jiang <yunhong.jiang@intel.com>
2015-02-11 15:41:54 -0800

    some minor changes

--- openstack-env/vm_devstack/image.sh
+++ openstack-env/vm_devstack/image.sh
@@ -3,5 +3,5 @@ rid=`glance image-create --name cirros.root --disk-format ari --container-format
 echo "ramdisk is $rid"
 kid=`glance image-create --name cirros.kernel --disk-format aki --container-format aki --file ./cirros-0.3.2-x86_64-vmlinuz`
 echo "kernel is $kid"
-image=`glance image-create --name cirros --disk-format ami --container-format ami --file ./cirros-0.3.2-x86_64-blank.img --property kernel_id\=$kid --property  ramdisk_id\=$rid`
-echo "Image is $image"
+#image=`glance image-create --name cirros --disk-format ami --container-format ami --file ./cirros-0.3.2-x86_64-blank.img --property kernel_id\=$kid --property  ramdisk_id\=$rid`
+#echo "Image is $image"
--- openstack-env/vm_devstack/start.sh
+++ openstack-env/vm_devstack/start.sh
@@ -2,14 +2,18 @@
 
 #The comand to login to the guest
 #nova ssh --private --login ubuntu -i ~/tmp/123.key controller
+`sudo cp pip.conf /etc/`
 `scp  yjiang5@otccloud06.sc.intel.com:/home/yjiang5/.ssh/* ~/.ssh/`
 `sudo chown ubuntu /opt`
 `sudo chgrp ubuntu /opt`
-`sudo scp yjiang5@172.25.110.34:/etc/apt/sources.list /etc/apt/`
-`sudo scp yjiang5@172.25.110.34:/etc/pip.conf /etc/`
+`sudo scp yjiang5@otccloud06.sc.intel.com:/etc/apt/sources.list /etc/apt/`
+`sudo scp yjiang5@otccloud06.sc.intel.com:/etc/pip.conf /etc/`
 `sudo apt-get update`
+#`sudo sh -c 'echo "auth_tcp = \"none\"" >>/etc/libvirt/libvirtd.conf'`
+#`sudo sh -c 'echo "listen_tcp = 1" >>/etc/libvirt/libvirtd.conf'`
+#`sudo sh -c 'echo "listen_tls = 0" >>/etc/libvirt/libvirtd.conf'`
 `sudo apt-get install git`
-`cd /opt; git clone ssh://yjiang5@172.25.110.34:/home/yjiang5/GITMIRROR/openstack-dev/devstack`
+`cd /opt; git clone ssh://yjiang5@otccloud06.sc.intel.com:/home/yjiang5/GITMIRROR/openstack-dev/devstack`
 `cd /opt/devstack; mkdir files/images/`
 `scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/images/* /opt/devstack/files/images/`
 `scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/get-pip.py /opt/devstack/files/`
