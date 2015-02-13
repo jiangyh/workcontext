0eb112d9234b7564207544ab81a0bbf458c13959
yunhong jiang <yunhong.jiang@intel.com>
2015-02-13 11:11:10 -0800

    Copy cirrost and Fedora image file tar

--- openstack-env/vm_devstack/start.sh
+++ openstack-env/vm_devstack/start.sh
@@ -7,7 +7,6 @@
 `sudo chown ubuntu /opt`
 `sudo chgrp ubuntu /opt`
 `sudo scp yjiang5@otccloud06.sc.intel.com:/etc/apt/sources.list /etc/apt/`
-`sudo scp yjiang5@otccloud06.sc.intel.com:/etc/pip.conf /etc/`
 `sudo apt-get update`
 #`sudo sh -c 'echo "auth_tcp = \"none\"" >>/etc/libvirt/libvirtd.conf'`
 #`sudo sh -c 'echo "listen_tcp = 1" >>/etc/libvirt/libvirtd.conf'`
@@ -16,5 +15,7 @@
 `cd /opt; git clone ssh://yjiang5@otccloud06.sc.intel.com:/home/yjiang5/GITMIRROR/openstack-dev/devstack`
 `cd /opt/devstack; mkdir files/images/`
 `scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/images/* /opt/devstack/files/images/`
+`scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/c*.gz /opt/devstack/files/`
+`scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/F*.qcow2 /opt/devstack/files/`
 `scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/get-pip.py /opt/devstack/files/`
 
