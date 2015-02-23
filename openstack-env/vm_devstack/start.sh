972f15241be90295b5a4d61d533f9730e30c190b
yunhong jiang <yunhong.jiang@intel.com>
2015-02-23 13:15:35 -0800

    add the pydistutils
    Signed-off-by: yunhong jiang <yunhong.jiang@intel.com>

--- openstack-env/vm_devstack/start.sh
+++ openstack-env/vm_devstack/start.sh
@@ -7,6 +7,11 @@ TOP_DIR=$(cd $(dirname "$0") && pwd)
 #The comand to login to the guest
 #nova ssh --private --login ubuntu -i ~/tmp/123.key controller
 sudo cp pip.conf /etc/
+# No idea why the virtualenv will use so old pip which only support pip.conf at per-user base
+mkdir ~/.pip/
+cp pip.conf ~/.pip/
+cp pydistutils.cfg ~/.pydistutils.cfg
+
 scp  yjiang5@otccloud06.sc.intel.com:/home/yjiang5/.ssh/* ~/.ssh/
 sudo chown ubuntu /opt
 sudo chgrp ubuntu /opt
@@ -17,11 +22,13 @@ sudo apt-get update
 #`sudo sh -c 'echo "listen_tls = 0" >>/etc/libvirt/libvirtd.conf'`
 sudo apt-get install git
 
+
 cd /opt;
 git clone $DEVSTACK_REPO
+cd /opt/devstack
 git checkout $DEVSTACK_BRANCH
 
-cd /opt/devstack; mkdir files/images/
+mkdir files/images/
 scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/images/* /opt/devstack/files/images/
 scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/c*.gz /opt/devstack/files/
 scp -r yjiang5@otccloud06.sc.intel.com:/home/yjiang5/work/openstack/devstack/files/F*.qcow2 /opt/devstack/files/
