9924bb1fd429beaf1339393c2acd161afc110792
yjiang5 <yjiang5@yjiang5-linux1.sc.intel.com>
2015-03-31 01:36:05 -0700

    Add the yum repo

---
+++ rt/yum.sh
@@ -0,0 +1,7 @@
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/os/x86_64/
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/updates/x86_64/
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/fasttrack/x86_64/
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/extras/x86_64/
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/cr/x86_64/
+sudo yum-config-manager --add-repo http://linux-ftp.sc.intel.com/pub/mirrors/centos/7.0.1406/centosplus/x86_64/
+
